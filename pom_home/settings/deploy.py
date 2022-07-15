eeecd537057f3dbae123bf04e7a8cc0669e4225f
duswns094 <duswns094@gmail.com>
2022-07-15 18:53:12 +0900

    secret modified

--- pom_home/settings/deploy.py
+++ pom_home/settings/deploy.py
@@ -1,11 +1,18 @@
 from .base import *
 
+def read_secret(secret_name):
+
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+
+    return secret
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-
-
 
 # Take environment variables from .env file
 environ.Env.read_env(
@@ -16,7 +23,7 @@ DEBUG = env('DEBUG')
 
 # Raises Django's ImproperlyConfigured
 # exception if SECRET_KEY not in os.environ
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -31,7 +38,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pom_database',
         'USER': 'admin',
-        'PASSWORD': 'vhdpagkrdnjs!',
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
         'HOST': 'mariadb',
         'POST': '3306',
     }
